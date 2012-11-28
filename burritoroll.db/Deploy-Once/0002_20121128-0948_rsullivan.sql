0c768d3b6dca30017bd6e09c5627d39c0be851cb
datachomp <rob@datachomp.com>
2012-11-28 09:49:12 -0600

    calorie to burrito table migration

---
+++ burritoroll.db/Deploy-Once/0002_20121128-0948_rsullivan.sql
@@ -0,0 +1,12 @@
+﻿-- <Migration ID="962ef2e0-8b4b-4ce6-9a8a-134e41d7ef6d" />
+
+
+
+
+PRINT N'Altering [dbo].[burritos]...';
+
+
+GO
+ALTER TABLE [dbo].[burritos]
+    ADD [calories] INT CONSTRAINT [DF_burritos_calories] DEFAULT 0 NOT NULL;
+
