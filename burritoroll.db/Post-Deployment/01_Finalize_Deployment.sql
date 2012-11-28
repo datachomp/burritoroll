b3f705a143825aa06b61b3c1ede52133c6832e5e
datachomp <rob@datachomp.com>
2012-11-28 10:49:03 -0600

    add taco stage data

--- burritoroll.db/Post-Deployment/01_Finalize_Deployment.sql
+++ burritoroll.db/Post-Deployment/01_Finalize_Deployment.sql
@@ -9,3 +9,18 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
+
+MERGE dbo.[tacos] AS Target
+USING (
+	SELECT  'Amigo' AS NAME ,2 AS price
+	UNION
+	SELECT  'Cheech' AS NAME ,2 AS price
+--SELECT * FROM dbo.tacos
+) AS Source
+ON (Target.name = Source.name AND Target.price = Source.price)
+--WHEN MATCHED THEN
+  --  UPDATE SET Target.price = Source.price
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT (NAME, price)
+    VALUES (Source.name, Source.price)
+OUTPUT source.name, SOURCE.price;
