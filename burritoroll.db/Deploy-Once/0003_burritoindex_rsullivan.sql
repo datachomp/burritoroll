da6a52f6948c72b48fb4390c7f2899821765f93e
datachomp <rob@datachomp.com>
2012-11-28 11:52:00 -0600

    some cool/hip migrations

---
+++ burritoroll.db/Deploy-Once/0003_burritoindex_rsullivan.sql
@@ -0,0 +1,11 @@
+﻿-- <Migration ID="84bd42b2-0cd7-416c-b252-3df8fafa80f6" />
+
+
+
+Print 'Create Index [IX_burriton_name] on [dbo].[burritos]'
+GO
+CREATE NONCLUSTERED INDEX [IX_burriton_name]
+	ON [dbo].[burritos] ([name])
+GO
+
+
---
+++ burritoroll.db/Deploy-Once/0004_20121128-0954_rsullivan.sql
@@ -0,0 +1,14 @@
+﻿-- <Migration ID="0f147295-f11e-454a-b202-67004ccb0851" />
+
+PRINT N'Creating [dbo].[getburritos]...';
+
+
+GO
+ALTER PROCEDURE [dbo].[getburritos]
+	@param1 int = 0
+	
+AS
+	SELECT @param1
+RETURN 0
+GO
+
