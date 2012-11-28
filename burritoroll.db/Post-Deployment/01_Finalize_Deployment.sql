250c41f63bfae9af9ccf11edf5d7c0bac519a409
datachomp <rob@datachomp.com>
2012-11-28 11:09:43 -0600

    Added burrito data

--- burritoroll.db/Post-Deployment/01_Finalize_Deployment.sql
+++ burritoroll.db/Post-Deployment/01_Finalize_Deployment.sql
@@ -24,3 +24,20 @@ WHEN NOT MATCHED BY TARGET THEN
     INSERT (NAME, price)
     VALUES (Source.name, Source.price)
 OUTPUT source.name, SOURCE.price;
+
+
+
+MERGE dbo.[burritos] AS Target
+USING (
+	SELECT  'Guapo' AS NAME , 4 AS price
+	UNION
+	SELECT  'Hombre' AS NAME , 10 AS price
+--SELECT * FROM dbo.tacos
+) AS Source
+ON (Target.name = Source.name AND Target.price = Source.price)
+--WHEN MATCHED THEN
+  --  UPDATE SET Target.price = Source.price
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT (NAME, price)
+    VALUES (Source.name, Source.price)
+OUTPUT source.name, SOURCE.price;
