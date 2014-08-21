be77c6a8a14865f6ddf38842c1164924221cd51b
Daniel Nolan <daniel@ready-roll.com>
2014-08-21 15:09:13 +1000

    Split post-deployment scripts into Deploy-Change migrations

---
+++ burritoroll.db/Deploy-Changes/Static Data/dbo.burritos.sql
@@ -0,0 +1,17 @@
+﻿
+MERGE dbo.[burritos] AS Target
+USING (
+	SELECT  'Guapo' AS NAME , 4 AS price
+	UNION ALL
+	SELECT  'Hombre' AS NAME , 10 AS price
+	UNION ALL
+	SELECT  'Bebe' AS NAME , 6 AS price
+--SELECT * FROM dbo.tacos
+) AS Source
+ON (Target.name = Source.name AND Target.price = Source.price)
+--WHEN MATCHED THEN
+  --  UPDATE SET Target.price = Source.price
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT (NAME, price)
+	VALUES (Source.name, Source.price)
+OUTPUT source.name, SOURCE.price;
---
+++ burritoroll.db/Deploy-Changes/Static Data/dbo.tacos.sql
@@ -0,0 +1,15 @@
+﻿
+MERGE dbo.[tacos] AS Target
+USING (
+	SELECT  'Amigo' AS NAME ,2 AS price
+	UNION ALL
+	SELECT  'Cheech' AS NAME ,2 AS price
+--SELECT * FROM dbo.tacos
+) AS Source
+ON (Target.name = Source.name AND Target.price = Source.price)
+--WHEN MATCHED THEN
+  --  UPDATE SET Target.price = Source.price
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT (NAME, price)
+	VALUES (Source.name, Source.price)
+OUTPUT source.name, SOURCE.price;
